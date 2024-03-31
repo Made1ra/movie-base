60a8ca2cf71a46f3f1457764f1a84054e7135308
Vadym Pavlyk <71404100+Made1ra@users.noreply.github.com>
2024-03-31 21:55:42 +0300

    Create definitions.ts

---
+++ app/lib/definitions.ts
@@ -0,0 +1,32 @@
+export type Rating = {
+    source: string;
+    value: string;
+};
+
+export type Movie = {
+    title: string;
+    year: string;
+    rated: string;
+    released: string;
+    runtime: string;
+    genre: string;
+    director: string;
+    writer: string;
+    actors: string;
+    plot: string;
+    language: string;
+    country: string;
+    awards: string;
+    poster: string;
+    ratings: Rating[];
+    metascore: string;
+    imdbRating: string;
+    imdbVotes: string;
+    imdbID: string;
+    type: string;
+    DVD: string;
+    boxOffice: string;
+    production: string;
+    website: string;
+    response: string;
+};
