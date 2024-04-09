f7cffa3e770cc20f18422b672fef0a20159b9f48
Vadym Pavlyk <71404100+Made1ra@users.noreply.github.com>
2024-04-09 21:20:54 +0300

    Update definitions.ts

--- app/lib/definitions.ts
+++ app/lib/definitions.ts
@@ -1,35 +1,35 @@
 export type Rating = {
-    source: string;
-    value: string;
+    Source: string;
+    Value: string;
 };
 
 export type Movie = {
-    title: string;
-    year: string;
-    rated: string;
-    released: string;
-    runtime: string;
-    genre: string;
-    director: string;
-    writer: string;
-    actors: string;
-    plot: string;
-    language: string;
-    country: string;
-    awards: string;
-    poster: string;
-    ratings: Rating[];
-    metascore: string;
+    Title: string;
+    Year: string;
+    Rated: string;
+    Released: string;
+    Runtime: string;
+    Genre: string;
+    Director: string;
+    Writer: string;
+    Actors: string;
+    Plot: string;
+    Language: string;
+    Country: string;
+    Awards: string;
+    Poster: string;
+    Ratings: Rating[];
+    Metascore: string;
     imdbRating: string;
     imdbVotes: string;
     imdbID: string;
-    type: string;
+    Type: string;
     DVD: string;
-    boxOffice: string;
-    production: string;
-    website: string;
-    response: string;
-    userRating: string;
+    BoxOffice: string;
+    Production: string;
+    Website: string;
+    Response: string;
+    UserRating: string;
 };
 
 export type User = {
