f0aaa59310a729b687ac5d0469e60a8aae6646b5
Vadym Pavlyk <71404100+Made1ra@users.noreply.github.com>
2024-04-09 21:20:49 +0300

    Update actions.ts

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,8 +4,21 @@ export async function getMovie(imdbID: string) {
     try {
         const API_KEY = process.env.API_KEY;
         const response = await fetch(`https://www.omdbapi.com/?i=${imdbID}&apikey=${API_KEY}`);
+        const data = await response.json();
 
-        return await response.json();
+        return data;
+    } catch (error) {
+        console.error(error);
+    }
+}
+
+export async function getMovies(searchQuery: string) {
+    try {
+        const API_KEY = process.env.API_KEY;
+        const response = await fetch(`https://www.omdbapi.com/?s=${searchQuery}&apikey=${API_KEY}`);
+        const data = await response.json();
+        
+        return data.Search;
     } catch (error) {
         console.error(error);
     }
@@ -15,8 +28,9 @@ export async function getPoster(imdbID: string) {
     try {
         const API_KEY = process.env.API_KEY;
         const response = await fetch(`https://www.img.omdbapi.com/?i=${imdbID}&apikey=${API_KEY}`);
+        const data = await response.json();
 
-        return await response.json();
+        return data;
     } catch (error) {
         console.error(error);
     }
