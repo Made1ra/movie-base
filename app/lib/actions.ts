4869f61fd081cdf2151624d6fe4aefdddf2575fe
Vadym Pavlyk <71404100+Made1ra@users.noreply.github.com>
2024-04-11 21:15:10 +0300

    Update actions.ts

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,6 +1,6 @@
 'use server';
 
-export async function getMovie(imdbID: string) {
+export async function getMovie(imdbID: string | string[]) {
     try {
         const API_KEY = process.env.API_KEY;
         const response = await fetch(`https://www.omdbapi.com/?i=${imdbID}&apikey=${API_KEY}`);
@@ -17,7 +17,7 @@ export async function getMovies(searchQuery: string) {
         const API_KEY = process.env.API_KEY;
         const response = await fetch(`https://www.omdbapi.com/?s=${searchQuery}&apikey=${API_KEY}`);
         const data = await response.json();
-        
+
         return data.Search;
     } catch (error) {
         console.error(error);
