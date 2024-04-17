f7eb6d3d80f5f8e5ef3ed9b2d78e611397da2732
Vadym Pavlyk <71404100+Made1ra@users.noreply.github.com>
2024-04-17 18:16:14 +0300

    Update utils.ts

--- app/lib/utils.ts
+++ app/lib/utils.ts
@@ -15,3 +15,20 @@ export const convertToHoursAndMinutes = (min: string) => {
 
     return result.trim();
 };
+
+export const formatNumber = (str: string) => {
+    const number = parseFloat(str.replace(/,/g, ''));
+
+    const K_THRESHOLD = 1000;
+    const M_THRESHOLD = 1000000;
+
+    if (number >= M_THRESHOLD) {
+        const roundedNumber = (number / M_THRESHOLD).toFixed(1);
+        return `${roundedNumber}M`;
+    } else if (number >= K_THRESHOLD) {
+        const roundedNumber = Math.round(number / K_THRESHOLD);
+        return `${roundedNumber}K`;
+    }
+
+    return str;
+};
