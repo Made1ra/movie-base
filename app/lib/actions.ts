cc851c851d25db971fc7d926a0f8eccb8639b17b
Vadym Pavlyk <71404100+Made1ra@users.noreply.github.com>
2024-04-26 21:11:28 +0300

    Remove unused function

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -1,6 +1,8 @@
 'use server';
 
 import { signOut } from '@/auth';
+import { db } from '@/app/server/db';
+import { watchlist } from '@/app/server/db/schema';
 
 export async function getMovie(imdbID: string | string[]) {
     try {
@@ -26,18 +28,10 @@ export async function getMovies(searchQuery: string) {
     }
 }
 
-export async function getPoster(imdbID: string) {
-    try {
-        const API_KEY = process.env.API_KEY;
-        const response = await fetch(`https://www.img.omdbapi.com/?i=${imdbID}&apikey=${API_KEY}`);
-        const data = await response.json();
-
-        return data;
-    } catch (error) {
-        console.error(error);
-    }
-}
-
 export async function signOutFromGoogle() {
     await signOut();
+}
+
+export async function postWatchlist(id: number, userID: string, movieID: string) {
+    await db.insert(watchlist).values({ id: id, userID: userID, movieID: movieID });
 }
