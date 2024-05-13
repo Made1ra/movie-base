5c26744661a12bd1c41b4add74f2bb3fc403b7cb
Vadym Pavlyk <71404100+Made1ra@users.noreply.github.com>
2024-05-13 10:54:30 +0300

    Add `Scroll to top` badge

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
 'use client';
 
+import { useState, useEffect, useCallback } from 'react';
+import { Session } from 'next-auth';
 import { Typography } from '@mui/material';
 import { useUserStore } from '@/app/stores/user-store';
 import { authorizeUser } from '@/app/lib/actions';
@@ -7,9 +9,9 @@ import SignIn from '@/app/components/sign-in';
 import BackButton from '@/app/components/back-button';
 import AccountMenu from '@/app/components/account-menu';
 import Search from '@/app/components/search';
+import { Badge } from '@/app/components/badge';
+import ChevronUpIcon from '@/app/components/chevron-up-icon';
 import Movies from '@/app/components/movies';
-import { useState, useEffect } from 'react';
-import { Session } from 'next-auth';
 
 export default function Home({
   searchParams
@@ -22,8 +24,16 @@ export default function Home({
   const query = searchParams?.query || '';
 
   const [session, setSession] = useState<Session | null | undefined>(null);
+  const [showBackToTop, setShowBackToTop] = useState(false);
 
   const { user, addUser } = useUserStore((state) => state);
+
+  const scrollToTop = useCallback(() => {
+    window.scrollTo({
+      top: 0,
+      behavior: 'smooth',
+    });
+  }, []);
 
   useEffect(() => {
     async function getSession() {
@@ -37,6 +47,22 @@ export default function Home({
     getSession();
   }, [addUser]);
 
+  useEffect(() => {
+    function handleScroll() {
+      if (window.scrollY > 100) {
+        setShowBackToTop(true);
+      } else {
+        setShowBackToTop(false);
+      }
+    }
+
+    window.addEventListener('scroll', handleScroll);
+
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   return (
     <div className="flex flex-col items-center text-center min-h-screen">
       <div className="mt-8">
@@ -49,6 +75,16 @@ export default function Home({
           </div>
           <AccountMenu image={user?.image} name={user?.name} id={user?.id} />
           <Search placeholder="Search movies..." />
+          {showBackToTop && (
+            <Badge
+              onClick={scrollToTop}
+              className="z-10 fixed top-4 left-[55rem] px-2 py-1 bg-neutral-50 text-black rounded-xl cursor-pointer shadow-xl text-base
+                    hover:bg-neutral-300 hover:text-black"
+            >
+              <ChevronUpIcon className="w-6 h-6 mr-2" />
+              Back to top
+            </Badge>
+          )}
           <Movies query={query} />
         </>
       ) : (
