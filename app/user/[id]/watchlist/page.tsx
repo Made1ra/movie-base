dba1938a8dd5766ca65dcfe156e26f811a9d2d5b
Vadym Pavlyk <71404100+Made1ra@users.noreply.github.com>
2024-06-13 16:03:15 +0300

    Add filtering

--- app/user/[id]/watchlist/page.tsx
+++ app/user/[id]/watchlist/page.tsx
@@ -11,85 +11,101 @@ import BackButton from '@/app/components/back-button';
 import AccountMenu from '@/app/components/account-menu';
 import { Badge } from '@/app/components/badge';
 import ChevronUpIcon from '@/app/components/icons/chevron-up-icon';
+import AdjustmentsHorizontalIcon from '@/app/components/icons/adjustments-horizontal-icon';
 import ArrowsUpDownIcon from '@/app/components/icons/arrows-up-down-icon';
 import MovieCard from '@/app/components/movie-card';
+import FilteringModal from '@/app/components/filtering-modal';
 
 export default function Watchlist() {
     const { id } = useParams();
     const { user } = useUserStore((state) => state);
     const [watchlistMovies, setWatchlistMovies] = useState<Movie[]>([]);
+    const [filteredMovies, setFilteredMovies] = useState<Movie[]>([]);
     const [showBackToTop, setShowBackToTop] = useState(false);
     const [sortOrder, setSortOrder] = useState<'desc' | 'asc'>('desc');
     const [value, setValue] = useState('Alphabetical');
+    const [isModalOpen, setIsModalOpen] = useState(false);
 
     function handleChange(event: SelectChangeEvent) {
         const newValue = event.target.value;
         setValue(newValue);
         setSortOrder('desc');
-        sortMovies(newValue, 'desc');
+        sortMovies(filteredMovies, newValue, 'desc');
     }
 
     function toggleSortOrder() {
         const newSortOrder = sortOrder === 'asc' ? 'desc' : 'asc';
         setSortOrder(newSortOrder);
-        sortMovies(value, newSortOrder);
+        sortMovies(filteredMovies, value, newSortOrder);
     }
 
-    function sortMovies(sortType: string, sortOrder: 'asc' | 'desc') {
+    function sortMovies(movies: Movie[], sortType: string, sortOrder: 'asc' | 'desc') {
         switch (sortType) {
             case 'Alphabetical':
-                sortByAlphabet(sortOrder);
+                sortByAlphabet(movies, sortOrder);
                 break;
             case 'IMDb rating':
-                sortByIMDbRating(sortOrder);
+                sortByIMDbRating(movies, sortOrder);
                 break;
             case 'Number of ratings':
-                sortByIMDbVotes(sortOrder);
+                sortByIMDbVotes(movies, sortOrder);
                 break;
             case 'Release date':
-                sortByReleaseDate(sortOrder);
+                sortByReleaseDate(movies, sortOrder);
                 break;
             case 'Runtime':
-                sortByRuntime(sortOrder);
+                sortByRuntime(movies, sortOrder);
                 break;
             default:
                 break;
         }
     }
 
-    function sortByAlphabet(order: 'asc' | 'desc') {
-        setWatchlistMovies(order === 'asc'
-            ? watchlistMovies.toSorted((a, b) => a.Title.localeCompare(b.Title))
-            : watchlistMovies.toSorted((a, b) => b.Title.localeCompare(a.Title))
+    function sortByAlphabet(movies: Movie[], order: 'asc' | 'desc') {
+        setFilteredMovies(order === 'desc'
+            ? movies.toSorted((a, b) => a.Title.localeCompare(b.Title))
+            : movies.toSorted((a, b) => b.Title.localeCompare(a.Title))
         );
     }
 
-    function sortByIMDbRating(order: 'asc' | 'desc') {
-        setWatchlistMovies(order === 'asc'
-            ? watchlistMovies.toSorted((a, b) => +a.imdbRating - +b.imdbRating)
-            : watchlistMovies.toSorted((a, b) => +b.imdbRating - +a.imdbRating)
+    function sortByIMDbRating(movies: Movie[], order: 'asc' | 'desc') {
+        setFilteredMovies(order === 'asc'
+            ? movies.toSorted((a, b) => +a.imdbRating - +b.imdbRating)
+            : movies.toSorted((a, b) => +b.imdbRating - +a.imdbRating)
         );
     }
 
-    function sortByIMDbVotes(order: 'asc' | 'desc') {
-        setWatchlistMovies(order === 'asc'
-            ? watchlistMovies.toSorted((a, b) => +a.imdbVotes.replace(/,/g, '') - +b.imdbVotes.replace(/,/g, ''))
-            : watchlistMovies.toSorted((a, b) => +b.imdbVotes.replace(/,/g, '') - +a.imdbVotes.replace(/,/g, ''))
+    function sortByIMDbVotes(movies: Movie[], order: 'asc' | 'desc') {
+        setFilteredMovies(order === 'asc'
+            ? movies.toSorted((a, b) => +a.imdbVotes.replace(/,/g, '') - +b.imdbVotes.replace(/,/g, ''))
+            : movies.toSorted((a, b) => +b.imdbVotes.replace(/,/g, '') - +a.imdbVotes.replace(/,/g, ''))
         );
     }
 
-    function sortByReleaseDate(order: 'asc' | 'desc') {
-        setWatchlistMovies(order === 'asc'
-            ? watchlistMovies.toSorted((a, b) => +a.Year - +b.Year)
-            : watchlistMovies.toSorted((a, b) => +b.Year - +a.Year)
+    function sortByReleaseDate(movies: Movie[], order: 'asc' | 'desc') {
+        setFilteredMovies(order === 'asc'
+            ? movies.toSorted((a, b) => +a.Year - +b.Year)
+            : movies.toSorted((a, b) => +b.Year - +a.Year)
         );
     }
 
-    function sortByRuntime(order: 'asc' | 'desc') {
-        setWatchlistMovies(order === 'asc'
-            ? watchlistMovies.toSorted((a, b) => +a.Runtime.split(' ')[0] - +b.Runtime.split(' ')[0])
-            : watchlistMovies.toSorted((a, b) => +b.Runtime.split(' ')[0] - +a.Runtime.split(' ')[0])
+    function sortByRuntime(movies: Movie[], order: 'asc' | 'desc') {
+        setFilteredMovies(order === 'asc'
+            ? movies.toSorted((a, b) => +a.Runtime.split(' ')[0] - +b.Runtime.split(' ')[0])
+            : movies.toSorted((a, b) => +b.Runtime.split(' ')[0] - +a.Runtime.split(' ')[0])
         );
+    }
+
+    function openModal() {
+        setIsModalOpen(true);
+    }
+
+    function closeModal() {
+        setIsModalOpen(false);
+    }
+
+    function applyFilters(filteredMovies: Movie[]) {
+        sortMovies(filteredMovies, value, sortOrder);
     }
 
     const scrollToTop = useCallback(() => {
@@ -107,6 +123,7 @@ export default function Watchlist() {
                 return movie;
             }));
             setWatchlistMovies(movies);
+            setFilteredMovies(movies);
         }
 
         fetchWatchlist();
@@ -143,7 +160,10 @@ export default function Watchlist() {
                     Back to top
                 </Badge>
             )}
-            <div className="flex items-center justify-center -mb-8">
+            <div className="flex items-center justify-center mt-4 -mb-8">
+                <Button onClick={openModal}>
+                    <AdjustmentsHorizontalIcon />
+                </Button>
                 <InputLabel>Sort by</InputLabel>
                 <Select
                     value={value}
@@ -161,7 +181,7 @@ export default function Watchlist() {
                 </Button>
             </div>
             <div className="flex flex-col items-center justify-center mb-8">
-                {watchlistMovies.map((movie: Movie) => (
+                {filteredMovies.map((movie: Movie) => (
                     <Link
                         href={`/title/${movie.imdbID}`}
                         key={movie.imdbID}
@@ -180,7 +200,17 @@ export default function Watchlist() {
                     </Link>
                 ))}
             </div>
-            <AccountMenu image={user?.image} name={user?.name} id={user?.id} />
+            <AccountMenu
+                image={user?.image}
+                name={user?.name}
+                id={user?.id}
+            />
+            <FilteringModal
+                open={isModalOpen}
+                movies={watchlistMovies}
+                onApplyFilters={applyFilters}
+                onClose={closeModal}
+            />
         </div>
     );
 }
