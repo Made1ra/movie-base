b3a45746280b1aa12b1f443fbc6bfa1b01791e91
Vadym Pavlyk <71404100+Made1ra@users.noreply.github.com>
2024-06-15 15:07:46 +0300

    Add chips with applied filters feature

--- app/components/filtering-modal.tsx
+++ app/components/filtering-modal.tsx
@@ -1,21 +1,27 @@
 'use client';
 
-import { useState, memo } from 'react';
+import { useState, memo, forwardRef, useImperativeHandle } from 'react';
 import { Modal, Box, Button, Chip, TextField, Typography } from '@mui/material';
 import { Movie } from '@/app/lib/definitions';
 import { getGenres, getTypes } from '@/app/lib/utils';
 
-function FilteringModal({
+const FilteringModal = forwardRef(({
     open,
     movies,
     onApplyFilters,
+    onShowFilters,
+    onDeleteFilter,
+    onResetFilters,
     onClose,
 }: {
     open: boolean;
     movies: Movie[];
     onApplyFilters: (filteredMovies: Movie[]) => void;
+    onShowFilters: (filters: Map<string, string[] | number>) => void;
+    onDeleteFilter: (filter: string) => void;
+    onResetFilters: () => void;
     onClose: () => void;
-}) {
+}, ref) => {
     const genres = getGenres(movies);
     const types = getTypes(movies);
     const [selectedGenres, setSelectedGenres] = useState<string[]>([]);
@@ -45,11 +51,94 @@ function FilteringModal({
         });
 
         onApplyFilters(filteredMovies);
+        showFilters();
         onClose();
     }
 
+    function showFilters() {
+        const map = new Map();
+        if (selectedGenres) {
+            map.set('genres', selectedGenres.map((genre) => genre.slice(0, genre.length - 4)));
+        }
+
+        if (selectedTypes) {
+            map.set('types', selectedTypes.map((type) => type.slice(0, type.length - 4)));
+        }
+
+        if (releaseYearFrom) {
+            map.set('releaseYearFrom', releaseYearFrom);
+        }
+
+        if (releaseYearTo) {
+            map.set('releaseYearTo', releaseYearTo);
+        }
+
+        if (ratingFrom) {
+            map.set('ratingFrom', ratingFrom);
+        }
+
+        if (ratingTo) {
+            map.set('ratingTo', ratingTo);
+        }
+
+        if (votesFrom) {
+            map.set('votesFrom', votesFrom);
+        }
+
+        if (votesTo) {
+            map.set('votesTo', votesTo);
+        }
+
+        onShowFilters(map);
+    }
+
+    useImperativeHandle(ref, () => ({
+        deleteFilter(filter: string) {
+            switch (filter) {
+                case 'genres':
+                    setSelectedGenres([]);
+                    break;
+                case 'types':
+                    setSelectedTypes([]);
+                    break;
+                case 'releaseYearFrom':
+                    setReleaseYearFrom(null);
+                    break;
+                case 'releaseYearTo':
+                    setReleaseYearTo(null);
+                    break;
+                case 'ratingFrom':
+                    setRatingFrom(null);
+                    break;
+                case 'ratingTo':
+                    setRatingTo(null);
+                    break;
+                case 'votesFrom':
+                    setVotesFrom(null);
+                    break;
+                case 'votesTo':
+                    setVotesTo(null);
+                    break;
+                default:
+                    break;
+            }
+
+            onDeleteFilter(filter);
+            handleApplyFilters();
+        }
+    }));
+
     function resetFilters() {
+        setSelectedGenres([]);
+        setSelectedTypes([]);
+        setReleaseYearFrom(null);
+        setReleaseYearTo(null);
+        setRatingFrom(null);
+        setRatingTo(null);
+        setVotesFrom(null);
+        setVotesTo(null);
         onApplyFilters(movies);
+        onResetFilters();
         onClose();
     }
 
@@ -195,6 +284,8 @@ function FilteringModal({
             </Box>
         </Modal>
     );
-}
+});
+
+FilteringModal.displayName = 'FilteringModal';
 
 export default memo(FilteringModal);
--- app/user/[id]/ratings/page.tsx
+++ app/user/[id]/ratings/page.tsx
@@ -1,9 +1,9 @@
 'use client';
 
-import { useState, useEffect, useCallback } from 'react';
+import { useState, useEffect, useCallback, useRef } from 'react';
 import { useParams } from 'next/navigation';
 import Link from 'next/link';
-import { InputLabel, Select, MenuItem, Button, SelectChangeEvent, Typography, Box } from '@mui/material';
+import { InputLabel, Select, MenuItem, Button, SelectChangeEvent, Typography, Box, Chip } from '@mui/material';
 import { Movie, Rating } from '@/app/lib/definitions';
 import { getRatings, getMovie } from '@/app/lib/actions';
 import { useUserStore } from '@/app/stores/user-store';
@@ -25,6 +25,16 @@ export default function Ratings() {
     const [sortOrder, setSortOrder] = useState<'desc' | 'asc'>('desc');
     const [value, setValue] = useState('Alphabetical');
     const [isModalOpen, setIsModalOpen] = useState(false);
+    const [genres, setGenres] = useState<string[]>([]);
+    const [types, setTypes] = useState<string[]>([]);
+    const [releaseYearFrom, setReleaseYearFrom] = useState<number | null>(null);
+    const [releaseYearTo, setReleaseYearTo] = useState<number | null>(null);
+    const [ratingFrom, setRatingFrom] = useState<number | null>(null);
+    const [ratingTo, setRatingTo] = useState<number | null>(null);
+    const [votesFrom, setVotesFrom] = useState<number | null>(null);
+    const [votesTo, setVotesTo] = useState<number | null>(null);
+
+    const filteringModalRef = useRef(null);
 
     function handleChange(event: SelectChangeEvent) {
         const newValue = event.target.value;
@@ -108,6 +118,114 @@ export default function Ratings() {
         sortMovies(filteredMovies, value, sortOrder);
     }
 
+    function showFilters(filters: Map<string, string[] | number>) {
+        filters.forEach((value, key) => {
+            switch (key) {
+                case 'genres':
+                    setGenres(value as string[]);
+                    break;
+                case 'types':
+                    setTypes(value as string[]);
+                    break;
+                case 'releaseYearFrom':
+                    setReleaseYearFrom(value as number);
+                    break;
+                case 'releaseYearTo':
+                    setReleaseYearTo(value as number);
+                    break;
+                case 'ratingFrom':
+                    setRatingFrom(value as number);
+                    break;
+                case 'ratingTo':
+                    setRatingTo(value as number);
+                    break;
+                case 'votesFrom':
+                    setVotesFrom(value as number);
+                    break;
+                case 'votesTo':
+                    setVotesTo(value as number);
+                    break;
+                default:
+                    break;
+            }
+        });
+    }
+
+    function handleDelete(filter: string) {
+        switch (filter) {
+            case 'genres':
+                setGenres([]);
+                break;
+            case 'types':
+                setTypes([]);
+                break;
+            case 'releaseYearFrom':
+                setReleaseYearFrom(null);
+                break;
+            case 'releaseYearTo':
+                setReleaseYearTo(null);
+                break;
+            case 'ratingFrom':
+                setRatingFrom(null);
+                break;
+            case 'ratingTo':
+                setRatingTo(null);
+                break;
+            case 'votesFrom':
+                setVotesFrom(null);
+                break;
+            case 'votesTo':
+                setVotesTo(null);
+                break;
+            default:
+                break;
+        }
+
+        const updatedFilters = {
+            genres: filter === 'genres' ? [] : genres,
+            types: filter === 'types' ? [] : types,
+            releaseYearFrom: filter === 'releaseYearFrom' ? null : releaseYearFrom,
+            releaseYearTo: filter === 'releaseYearTo' ? null : releaseYearTo,
+            ratingFrom: filter === 'ratingFrom' ? null : ratingFrom,
+            ratingTo: filter === 'ratingTo' ? null : ratingTo,
+            votesFrom: filter === 'votesFrom' ? null : votesFrom,
+            votesTo: filter === 'votesTo' ? null : votesTo,
+        };
+
+        const filteredMovies = ratedMovies.filter((movie) => {
+            const movieGenres = movie.Genre.split(', ');
+            const movieType = movie.Type;
+            const releaseYear = parseInt(movie.Year);
+            const imdbRating = parseFloat(movie.imdbRating);
+            const imdbVotes = parseInt(movie.imdbVotes.replace(/,/g, ''));
+
+            const genreMatch = !updatedFilters.genres.length || updatedFilters.genres.some((genre) => movieGenres.includes(genre));
+            const typeMatch = !updatedFilters.types.length || updatedFilters.types.some((type) => type.toLowerCase() === movieType.toLowerCase());
+            const releaseYearMatch = (!updatedFilters.releaseYearFrom || releaseYear >= updatedFilters.releaseYearFrom) &&
+                (!updatedFilters.releaseYearTo || releaseYear <= updatedFilters.releaseYearTo);
+            const ratingMatch = (!updatedFilters.ratingFrom || imdbRating >= updatedFilters.ratingFrom) &&
+                (!updatedFilters.ratingTo || imdbRating <= updatedFilters.ratingTo);
+            const votesMatch = (!updatedFilters.votesFrom || imdbVotes >= updatedFilters.votesFrom) &&
+                (!updatedFilters.votesTo || imdbVotes <= updatedFilters.votesTo);
+
+            return genreMatch && typeMatch && releaseYearMatch && ratingMatch && votesMatch;
+        });
+
+        setFilteredMovies(filteredMovies);
+        applyFilters(filteredMovies);
+    }
+
+    function resetFilters() {
+        setGenres([]);
+        setTypes([]);
+        setReleaseYearFrom(null);
+        setReleaseYearTo(null);
+        setRatingFrom(null);
+        setRatingTo(null);
+        setVotesFrom(null);
+        setVotesTo(null);
+    }
+
     const scrollToTop = useCallback(() => {
         window.scrollTo({
             top: 0,
@@ -180,13 +298,73 @@ export default function Ratings() {
                     <ArrowsUpDownIcon />
                 </Button>
             </div>
-            <div className="flex flex-col items-center justify-center mb-8">
+            <div className="flex flex-col items-center justify-center">
                 {filteredMovies ? (
                     <>
-                        <Box className="mt-6 -mb-10">
+                        <Box className="mt-8 -mb-10">
                             <Typography variant="subtitle1">
                                 {`${filteredMovies.length === 1 ? '1 title' : `${filteredMovies.length} titles`}`}
                             </Typography>
+                        </Box>
+                        <Box className="mt-10 mb-2 flex justify-center gap-2 max-w-96 flex-wrap max-sm:max-w-80">
+                            {genres.map((genre) => (
+                                <Chip
+                                    key={genre}
+                                    label={`${genre}`}
+                                    onDelete={() => handleDelete('genres')}
+                                    className="hover:cursor-pointer hover:bg-neutral-300"
+                                />
+                            ))}
+                            {types.map((type) => (
+                                <Chip
+                                    key={type}
+                                    label={type}
+                                    onDelete={() => handleDelete('types')}
+                                    className="hover:cursor-pointer hover:bg-neutral-300"
+                                />
+                            ))}
+                            {releaseYearFrom && (
+                                <Chip
+                                    label={`Release year: before ${releaseYearFrom}`}
+                                    onDelete={() => handleDelete('releaseYearFrom')}
+                                    className="hover:cursor-pointer hover:bg-neutral-300"
+                                />
+                            )}
+                            {releaseYearTo && (
+                                <Chip
+                                    label={`Release year: after ${releaseYearTo}`}
+                                    onDelete={() => handleDelete('releaseYearTo')}
+                                    className="hover:cursor-pointer hover:bg-neutral-300"
+                                />
+                            )}
+                            {ratingFrom && (
+                                <Chip
+                                    label={`User rating: ${ratingFrom} or above`}
+                                    onDelete={() => handleDelete('ratingFrom')}
+                                    className="hover:cursor-pointer hover:bg-neutral-300"
+                                />
+                            )}
+                            {ratingTo && (
+                                <Chip
+                                    label={`User rating: ${ratingTo} or below`}
+                                    onDelete={() => handleDelete('ratingTo')}
+                                    className="hover:cursor-pointer hover:bg-neutral-300"
+                                />
+                            )}
+                            {votesFrom && (
+                                <Chip
+                                    label={`Number of votes: ${votesFrom} or above`}
+                                    onDelete={() => handleDelete('votesFrom')}
+                                    className="hover:cursor-pointer hover:bg-neutral-300"
+                                />
+                            )}
+                            {votesTo && (
+                                <Chip
+                                    label={`Number of votes: ${votesTo} or below`}
+                                    onDelete={() => handleDelete('votesTo')}
+                                    className="hover:cursor-pointer hover:bg-neutral-300"
+                                />
+                            )}
                         </Box>
                         {filteredMovies.map((movie: Movie) => (
                             <Link
@@ -219,9 +397,13 @@ export default function Ratings() {
                 id={user?.id}
             />
             <FilteringModal
+                ref={filteringModalRef}
                 open={isModalOpen}
                 movies={ratedMovies}
                 onApplyFilters={applyFilters}
+                onShowFilters={showFilters}
+                onDeleteFilter={handleDelete}
+                onResetFilters={resetFilters}
                 onClose={closeModal}
             />
         </div>
